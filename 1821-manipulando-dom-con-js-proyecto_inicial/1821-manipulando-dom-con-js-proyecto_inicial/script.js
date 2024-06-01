61faf687c50fd409e099720a2a1f7e64cd7d85fd
Cecil <123985852+Cecil78@users.noreply.github.com>
2024-06-01 10:16:20 -0400

    Add files via upload

---
+++ 1821-manipulando-dom-con-js-proyecto_inicial/1821-manipulando-dom-con-js-proyecto_inicial/script.js
@@ -0,0 +1,45 @@
+const btn = document.querySelector("button");
+console.log(btn);
+
+const createTask = (evento) => {
+    evento.preventDefault()
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value = '';
+    //backticks
+    console.log(checkComplete());
+    const taskContent= document.createElement ("div"); 
+
+    const titleTask= document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    const content = `
+    <i class = "fas fa-trash-alt trashIcon icon"></i>`
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
+    list.appendChild(task); 
+
+
+    console.log(content);
+}
+
+//Arrow functions o funciones anonimas 
+btn.addEventListener('click', createTask);
+
+const checkComplete= () => {
+    const i = document.createElement("i"); 
+    i.classList.add("far", "fa-check-square", "icon" );
+    i.addEventListener("click", completeTask);
+    return i; 
+}
+
+const completeTask = (event) => { 
+     console.log(event);
+     const item = event.target;
+     item.parentNode.remove();
+}
